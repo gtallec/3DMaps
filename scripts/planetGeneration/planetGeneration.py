e37e527cca1d2ea4670aa892a0c52df592e7a9f3
fdexnr@gmail.com <fdexnr@gmail.com>
2018-02-16 20:18:12 +0100

    premiere integration

--- scripts/planetGeneration/planetGeneration.py
+++ scripts/planetGeneration/planetGeneration.py
@@ -5,7 +5,8 @@ import bpy
 import sys
 sys.path.append('/home/antoine/3DMaps/scripts/planetGeneration')
 import Planet
-
+sys.path.append('/home/antoine/3DMaps/landscape')
+import Globe
 
 
 # Empty the scene
@@ -13,18 +14,20 @@ bpy.ops.object.select_all(action='DESELECT')
 bpy.ops.object.select_all()
 bpy.ops.object.delete()
 
-alts = np.zeros(480)
-alts.shape = (15, 32)
-idx = 0
-ex = 0
-while idx < 32:
-    alts[1][idx] = ex
-    idx += 1
-    ex += 0.1
-
-idx = 0
-while idx < 32:
-    alts[5][idx] = 1
-    idx += 1
+#instanciate a dict with key the type of landscape and value the (altb,alth,gradient,lengthParameter)
+dictOfLandscapes = {"sea" : (-4000,-2000,10),"transition" : (-2000,0,200), "plain" : (0,1000,10),"mountain" : (1000,7000,500)}
+width = 256
+height = 128
     
-newPlanet = Planet.Planet("test", 16, 32, alts,1)
+globe = Globe.Globe(5000,dictOfLandscapes, height, width)
+
+alts = globe.getAltitudeMatrix()
+lat = 0
+while lat < 128:
+    lon = 0
+    while lon < 256:
+        alts[lat][lon]=alts[lat][lon]/70000
+        lon+=1
+    lat+=1
+    
+newPlanet = Planet.Planet("test", 128, 256, alts,1)
