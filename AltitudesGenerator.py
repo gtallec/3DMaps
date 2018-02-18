802e448e480038dfe1d23bd8678fa6fb98081ffc
TALLEC <gauthier.tallec996@gmail.com>
2018-02-18 13:38:52 +0100

    first try with white noise

--- AltitudesGenerator.py
+++ AltitudesGenerator.py
@@ -3,17 +3,17 @@
 import numpy as np
 
 class AltitudesGenerator:
-    """ Sert a generer une matrice aleatoire d'altitudes a partir d'un 
+    """ Sert a generer une matrice aleatoire d'altitudes a partir d'un
     nombre de latitudes et de longitudes"""
-    
+
     def __init__(self):
         pass
-    
+
     """La fonction qui genere les altitudes
     Doit retourner une np.array de forme (latNb-1, lonNb)"""
-    
+
     def generateAltitudes(self, latNb, lonNb):
-        altitudes = np.zeros((latNb-1)*lonNb)
-        altitudes.shape = (latNb-1, lonNb)
+        altitudes = np.zeros(shape = (latNb-1,lonNb))
+        whiteNoise = np.random.normal(size = (latNb-1,lonNb) )
+        altitudes = whiteNoise
         return altitudes
-        
