3a453a8031936ad067c1839959902071e250b050
TALLEC <gauthier.tallec996@gmail.com>
2018-02-24 17:43:50 +0100

    putting package structure into perlinNoise folder

--- perlinNoise/Test.py
+++
@@ -1,4 +0,0 @@
-from PerlinMatrixGenerator import PerlinMatrixGenerator
-
-perlinGenerator = PerlinMatrixGenerator(5,5)
-print(perlinGenerator.generate())
---
+++ perlinNoise/__init__.py
@@ -0,0 +1,1 @@
+from .PerlinMatrixGenerator import PerlinMatrixGenerator
